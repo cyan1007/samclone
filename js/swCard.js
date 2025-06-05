12f8732350b0424b1a8f1a100d43c664d4d12aa4
cyan1007 <ymin803@naver.com>
2025-06-05 12:19:46 +0900

    main 부분과 카드부분 코드 수정

--- js/swCard.js
+++ js/swCard.js
@@ -1,8 +1,29 @@
 const swCard = new Swiper(".sw-card", {
-  slidesPerView: 3,
+  // 기본값
+  centeredSlides: false,
+  slidesPerView: "auto",
   spaceBetween: 30,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  //  반응형
+  breakpoints: {
+    0: {
+      centeredSlides: true,
+      spaceBetween: 20,
+    },
+    768: {
+      centeredSlides: false,
+      spaceBetween: 30,
+    },
+    1220: {
+      centeredSlides: false,
+      spaceBetween: 30,
+    },
   },
 });
